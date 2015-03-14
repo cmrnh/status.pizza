438fd714991285e1345f0dc58a06cd12fda359ff
Cameron Hendrix <cameron@leaptank.org>
2015-03-14 14:35:21 -0400

    added status code generator

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello Huge!')
-})
+// Configure view engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/:statuscode([0-9]{3})', function(req, res, next) {
+	response.send('Hello ' + req.params.statuscode);
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
