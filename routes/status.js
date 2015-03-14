fed5828a66357bbc2663aa9548579175910aeaf9
Cameron Hendrix <cameron@leaptank.org>
2015-03-14 15:24:04 -0400

    fix

--- routes/status.js
+++ routes/status.js
@@ -2,8 +2,8 @@ var validCodes = require('../resources/valid-codes.json');
 
 var send = function(req, res, next) {
 	var statusCode = req.params.statusCode;
-	var code = validCodes[statusCode];
-	if (!code) code = validCodes[statusCode];
+	var validCode = validCodes[statusCode];
+	if (!validCode) validCode = validCodes[statusCode];
 	res.render('status', validCode);
 };
 
