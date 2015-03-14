b6cbae2bb3eb03af695fa6b820baeb1102347924
Cameron Hendrix <cameron@leaptank.org>
2015-03-14 14:40:49 -0400

    fixed bug

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-// Configure view engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
