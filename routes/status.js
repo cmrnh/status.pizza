70a1689cd24fd781a1d00577ff8ee2a40ff89b8e
Cameron Hendrix <cameron@leaptank.org>
2015-03-14 15:44:02 -0400

    added randomizer

--- routes/status.js
+++ routes/status.js
@@ -3,7 +3,7 @@ var validCodes = require('../resources/valid-codes.json');
 var send = function(req, res, next) {
 	var statusCode = req.params.statusCode;
 	var validCode = validCodes[statusCode];
-	if (!validCode) validCode = validCodes[statusCode];
+	if (!validCode) validCode = validCodes["404"];
 	res.render('status', validCode);
 };
 
