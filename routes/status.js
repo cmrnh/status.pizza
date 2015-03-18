015d360be36310139bcb72622a044354dd0bfd4a
Cameron Hendrix <cameron@leaptank.org>
2015-03-17 21:55:15 -0400

    removed redundant title key in valid-codes.json — now creating title the status code and description

--- routes/status.js
+++ routes/status.js
@@ -3,8 +3,11 @@ var validCodes = require('../resources/valid-codes.json');
 var send = function(req, res, next) {
 	var statusCode = req.params.statusCode;
 	var validCode = validCodes[statusCode];
-	if (!validCode) validCode = validCodes["404"];
-	res.render('status', validCode);
+	if (!validCode) validCode = validCodes['404'];
+	res.render('status', {
+		'title': statusCode + ' ' + validCode.description,
+		'image': validCode.image
+	});
 };
 
 module.exports.send = send;
