c73f3c7f47e0712e79cdcab428181b4559a5d061
Cameron Hendrix <cameron@leaptank.org>
2015-03-17 21:56:53 -0400

    fixed edge case eerroror when requesting non-existent status code

--- routes/status.js
+++ routes/status.js
@@ -3,7 +3,10 @@ var validCodes = require('../resources/valid-codes.json');
 var send = function(req, res, next) {
 	var statusCode = req.params.statusCode;
 	var validCode = validCodes[statusCode];
-	if (!validCode) validCode = validCodes['404'];
+	if (!validCode) {
+		statusCode = '404';
+		validCode = validCodes['404'];
+	} 
 	res.render('status', {
 		'title': statusCode + ' ' + validCode.description,
 		'image': validCode.image
